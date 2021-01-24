1e320fa6f3735bb2b4d390b22557043ea6ac15eb
Kenna Wang <kenna.wang6@gmail.com>
2021-01-24 13:16:57 -0600

    survey

--- qwertracker/app/src/main/java/com/example/qwertracker/ui/questions/Question4Fragment.java
+++ qwertracker/app/src/main/java/com/example/qwertracker/ui/questions/Question4Fragment.java
@@ -4,24 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.qwertracker.MainActivity;
 import com.example.qwertracker.R;
 import com.example.qwertracker.data.AppDatabase;
 import com.example.qwertracker.data.SurveyResponseDao;
-import com.example.qwertracker.ui.dashboard.DashboardFragment;
-import com.example.qwertracker.ui.dashboard.DashboardViewModel;
 
-public class Question1Fragment extends Fragment {
+public class Question4Fragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,19 +23,18 @@ public class Question1Fragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_question1, container, false);
+        View root = inflater.inflate(R.layout.fragment_question4, container, false);
         return root;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        Button next = view.findViewById(R.id.next1);
-        next.setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.next4).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(Question1Fragment.this)
-                        .navigate(R.id.action_Question1Fragment_to_Question2Fragment);
+                NavHostFragment.findNavController(Question4Fragment.this)
+                        .navigate(R.id.action_Question4Fragment_to_GraphFragment);
             }
         });
     }
